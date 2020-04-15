ed0b560ad4dfbad1223fc4cc104970035d2acaa3
Tran Nguyen Chi Nhan <39859635+tncn1122@users.noreply.github.com>
2020-04-15 15:02:09 +0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -6,23 +6,38 @@ import pyautogui as pag
 from minimax import miniMax
 
 #FHD_____________________________
-mouseClickPosX = [330, 480, 630]
-mouseClickPosY = [355, 505, 655]
-
-checkPixelPosY = [112, 262, 412]
-checkPixelPosX = [54, 204, 354]
-
-pixelPos = [82, 232, 382]
+#imgSize = 465
+#regionLeft = 247
+#regionTop = 271
+#mouseClickPosX = [330, 480, 630]
+#mouseClickPosY = [355, 505, 655]
+#checkPixelPosY = [112, 262, 412]
+#checkPixelPosX = [54, 204, 354]
+#pixelPos = [82, 232, 382]
+#restartClick = 403
 #________________________________
  
+#HD______________________________155
+imgSize = 450
+regionLeft = 191
+regionTop = 128
+mouseClickPosX = [267, 417, 567]
+mouseClickPosY = [203, 353, 503]
 
+checkPixelPosY = [105, 255, 405]
+checkPixelPosX = [47, 197, 347]
+
+pixelPos = [70, 220, 370]
+restartClick = 339
+#________________________________
+#img = np.array(pag.screenshot("test.png", region = (regionLeft, regionTop, imgSize, imgSize)))
 while(1):
     board = np.zeros((3, 3))
     playing = True
     print("Playing!")
     while(playing):
         time.sleep(1)
-        img = np.array(pag.screenshot(region = (247, 271, 465, 465)))
+        img = np.array(pag.screenshot(region = (regionLeft, regionTop, imgSize, imgSize)))
         emptySlot = 0
         for i in range(3):
             for j in range(3):
@@ -35,8 +50,7 @@ while(1):
                         board[i][j] = 2 #O
                 else:
                     #count the number of empty slots
-                    emptySlot += 1      
-        
+                    emptySlot += 1   
         if (isFinish(board) != 0 or emptySlot == 0):
             break
 
@@ -44,12 +58,12 @@ while(1):
         nextClick = miniMax(board, 7, emptySlot)
 
         print("next step: ", nextClick)
-        pag.click(x = mouseClickPosX[nextClick[1]], y = mouseClickPosY[nextClick[0]])
-        
+        pag.click(x = mouseClickPosX[nextClick[1]], y = mouseClickPosY[nextClick[0]])   
+    
     for sec in range(3, 0, -1):
         print(sec, "...")
         time.sleep(1)
-    pag.click(x = 403, y = 403)
+    pag.click(x = restartClick, y = restartClick)
 
 
     
